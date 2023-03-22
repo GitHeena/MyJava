9cda744e42c5cf5ec67a9c1c688b6a8739610b4d
GitHeena <128645487+GitHeena@users.noreply.github.com>
2023-03-22 16:32:53 -0500

    Add files via upload

---
+++ FindStringCount.java
@@ -0,0 +1,10 @@
+
+public class FindStringCount {
+
+	public static void main(String[] args) {
+		String a = "WorldIsSoMean";
+		System.out.println("The no of characters in a given String is: "+ a.length());
+
+	}
+
+}
